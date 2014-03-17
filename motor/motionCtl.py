936e1c934392ed088607ab7cc26100839df10040
Hao Men <hao.men@gmail.com>
2014-03-16 22:08:22 -0400

    next is motion control, without motion control nothing is moving accurately

---
+++ motor/motionCtl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#motion control module
+
+class motionCtl:
+
+#initalize class, load motor class
+    def __init__(self):
+        
+#vehicle motion initialization
+
+
+#steering control
+
+
+#velocity control
+
+
+#emergency stop
+
+
+#preception?
+
+
+#PID control for accuracy?
